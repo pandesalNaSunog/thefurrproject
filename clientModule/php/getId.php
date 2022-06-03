5b97387a3f7bc6cb7f8b829a1f018e8c93d7f277
pandesalNaSunog <floresjem8@gmail.com>
2022-06-03 18:46:47 +0800

    latest

--- clientModule/php/getId.php
+++ clientModule/php/getId.php
@@ -1,8 +1,16 @@
 <?php
+    
+    include('connection.php');
+    $con = connect();
     if(!isset($_SESSION)){
         session_start();
+        if(isset($_SESSION['client_email'])){
+            $email = $_SESSION['client_email'];
+            $query = "SELECT * FROM users WHERE email = '$email'";
+            $user = $con->query($query) or die($con->error);
+            $row = $user->fetch_assoc();
 
-        if(isset($_SESSION['client_id'])){
+            $_SESSION['client_id'] = $row['id'];
             echo $_SESSION['client_id'];
         }else{
             echo 'index.html';
