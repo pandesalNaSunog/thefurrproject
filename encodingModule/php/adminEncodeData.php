3617dbfe4bd7cfa6db6d7d187d6e57e2aeec4f42
pandesalNaSunog <floresjem8@gmail.com>
2022-07-06 15:55:56 +0800

    latest

--- encodingModule/php/adminEncodeData.php
+++ encodingModule/php/adminEncodeData.php
@@ -1,4 +1,6 @@
 <?php
+    date_default_timezone_set('Asia/Manila');
+    $today = date('Y-m-d H:i:s');
     include('connection.php');
     $con = connect();
 
@@ -15,6 +17,19 @@
         $weight = $_POST['pet_weight'];
         $sex = $_POST['pet_sex'];
 
-        echo json_encode($_POST);
+        //insert appointment history
+
+        $query = "INSERT INTO appointments(`client_id`,`doctor_id`,`date`,`time`,`concern`,`created_at`,`updated_at`)VALUES('$userId','$vetId','$appointmentDate','null','$clientConcern','$today','$today')";
+        $con->query($query) or die($con->error);
+
+        //insert patient details
+        $query = "SELECT * FROM appointments WHERE id = LAST_INSERT_ID()";
+        $appointment = $con->query($query) or die($con->error);
+        $appointmentRow = $appointment->fetch_assoc();
+        $appointmentId = $appointmentRow['id'];
+
+        $query = "INSERT INTO patient_details(`appointment_id`,`client_id`,`pet_name`,`age`,`breed`,`species`,`weight`,`sex`,`created_at`,`updated_at`)VALUES('$appointmentId','$userId','$petName','$age','$breed','$species','$weight','$sex','$today','$today')";
+        $con->query($query) or die($con->error);
+        echo 'ok';
     }
 ?>
