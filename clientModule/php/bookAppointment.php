6714c16b562c0ec708ef33815a4a8d277ebdcb43
pandesalNaSunog <floresjem8@gmail.com>
2022-07-06 12:36:12 +0800

    latest

--- clientModule/php/bookAppointment.php
+++ clientModule/php/bookAppointment.php
@@ -10,30 +10,20 @@
         $date = $_POST['date'];
         $time = $_POST['time'];
         $concern = $_POST['concern'];
+        $query = "INSERT INTO appointments(`status`,`client_id`,`doctor_id`,`date`,`time`,`concern`,`created_at`,`updated_at`)VALUES('Booked','$clientId','$doctorId','$date','$time','$concern','$createdAt','$createdAt')";
+        $con->query($query) or die($con->error);
+        
 
+        $query = "SELECT * FROM users WHERE id = '$clientId'";
+        $user = $con->query($query) or die($con->error);
+        $userRow = $user->fetch_assoc();
 
-        $postedDate = date_create($date);
-        $todayDateObject = date_create($todaysDate);
-        $dateDiff = date_diff($postedDate, $todayDateObject);
-        $diffresult = $dateDiff->format('%R');
-        if($diffresult == "-"){
-            echo 'invalid date';
-        }else{
-            $query = "INSERT INTO appointments(`status`,`client_id`,`doctor_id`,`date`,`time`,`concern`,`created_at`,`updated_at`)VALUES('Booked','$clientId','$doctorId','$date','$time','$concern','$createdAt','$createdAt')";
-            $con->query($query) or die($con->error);
-            
-    
-            $query = "SELECT * FROM users WHERE id = '$clientId'";
-            $user = $con->query($query) or die($con->error);
-            $userRow = $user->fetch_assoc();
-    
-            $clientName = $userRow['name'];
-    
-            $query = "SELECT * FROM doctors WHERE id = '$doctorId'";
-            $appointment = $con->query($query) or die($con->error);
-            $row = $appointment->fetch_assoc();
-    
-            echo json_encode(array('appointment' => $row, 'client_name' => $clientName));
-        }
+        $clientName = $userRow['name'];
+
+        $query = "SELECT * FROM doctors WHERE id = '$doctorId'";
+        $appointment = $con->query($query) or die($con->error);
+        $row = $appointment->fetch_assoc();
+
+        echo json_encode(array('appointment' => $row, 'client_name' => $clientName));
     }
 ?>
