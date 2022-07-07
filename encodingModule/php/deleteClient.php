2160e542699d38116a9625c88f778bfcb3798ec6
pandesalNaSunog <floresjem8@gmail.com>
2022-07-07 10:40:11 +0800

    latest

--- encodingModule/php/deleteClient.php
+++ encodingModule/php/deleteClient.php
@@ -4,6 +4,8 @@
 
     if(isset($_POST)){
         $userId = $_POST['user_id'];
+        $query = "DELETE FROM users WHERE id = '$userId'";
+        $con->query($query) or die($con->query);
         echo 'ok';
     }
 ?>
