f499b260b1e4bf0e9841b8f57c081ac48791fc28
pandesalNaSunog <floresjem8@gmail.com>
2022-07-07 19:17:06 +0800

    latest

--- encodingModule/php/updateClient.php
+++ encodingModule/php/updateClient.php
@@ -4,11 +4,11 @@
 
     if(isset($_POST)){
         $userId = $_POST['user_id'];
-        // $name = $_POST['client_name'];
-        // $code = $_POST['client_code'];
-        // $contact = $_POST['client_contact'];
-        // $query = "UPDATE users SET name = '$name', client_code = '$code', contact_no = '$contact' WHERE id = '$userId'";
-        // $con->query($query) or die($con->error);
-        echo $userId;
+        $name = $_POST['client_name'];
+        $code = $_POST['client_code'];
+        $contact = $_POST['client_contact'];
+        $query = "UPDATE users SET name = '$name', client_code = '$code', contact_no = '$contact' WHERE id = '$userId'";
+        $con->query($query) or die($con->error);
+        echo 'ok';
     }
 ?>
