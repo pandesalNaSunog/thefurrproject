b8947a1500a5fc255a7381d5e2253e0ea93054a0
pandesalNaSunog <floresjem8@gmail.com>
2022-07-08 19:52:41 +0800

    latest

--- encodingModule/php/getClientPets.php
+++ encodingModule/php/getClientPets.php
@@ -5,5 +5,35 @@
 
     if(isset($_GET)){
         $appointmentId = $_GET['appointment_id'];
+        $query = "SELECT * FROM pet_age_and_sex WHERE appointment_id = '$appointmentId'";
+        $pet = $con->query($query) or die($con->error);
+        $data = array();
+        while($row = $pet->fetch_assoc()){
+            $data[] = $row;
+        }
+        $response = array();
+        foreach($data as $dataItem){
+            $petId = $dataItem['pet_id'];
+            $query = "SELECT * FROM patient_details WHERE pet_id = '$petId'";
+            $patient = $con->query($query) or die($con->error);
+            $patientRow = $patient->fetch_assoc();
+            $name = $patientRow['pet_name'];
+            $breed = $patientRow['breed'];
+            $species = $patientRow['species'];
+            $sex = $patientRow['sex'];
+            $weight = $dataItem['weight'];
+            $age = $dataItem['age'];
+
+            $response[] = array(
+                'name' => $name,
+                'breed' => $breed,
+                'species' => $species,
+                'sex' => $sex,
+                'weight' => $weight,
+                'age' => $age
+            );
+        }
+
+        echo json_encode($response);
     }
 ?>
