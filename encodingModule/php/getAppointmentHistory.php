9298a463e69dbd2e168e2a91385080fd81585fc8
pandesalNaSunog <floresjem8@gmail.com>
2022-07-08 12:29:07 +0800

    latest

--- encodingModule/php/getAppointmentHistory.php
+++ encodingModule/php/getAppointmentHistory.php
@@ -26,17 +26,6 @@
             $query = "SELECT * FROM doctors WHERE id = '$vetId'";
             $doctor = $con->query($query) or die($con->error);
             $doctorRow = $doctor->fetch_assoc();
-
-
-            $query = "SELECT * FROM patient_details WHERE appointment_id = '$appointmentId'";
-            $patient = $con->query($query) or die($con->error);
-            $patientRow = $patient->fetch_assoc();
-            $petName = $patientRow['pet_name'];
-            $age = $patientRow['age'];
-            $breed = $patientRow['breed'];
-            $species = $patientRow['species'];
-            $weight = $patientRow['weight'];
-            $sex = $patientRow['sex'];
             $clientName = $userRow['name'];
             $attendingVet = $doctorRow['name'];
             $clientConcern = $appointmentItem['concern'];
@@ -45,12 +34,6 @@
                 'client_name' => $clientName,
                 'attending_vet' => $attendingVet,
                 'client_concern' => $clientConcern,
-                'pet_name' => $petName,
-                'pet_age' => $age,
-                'pet_breed' => $breed,
-                'pet_species' => $species,
-                'pet_weight' => $weight,
-                'pet_sex' => $sex,
                 'appointment_date' => $appointmentDate
             );
         }
