2a6fde616028da2eca8ac9befe52987d31d56ef2
pandesalNaSunog <floresjem8@gmail.com>
2022-07-08 19:21:02 +0800

    latest

--- encodingModule/php/insertPet.php
+++ encodingModule/php/insertPet.php
@@ -13,15 +13,21 @@
         $species = $_POST['species'];
         $weight = $_POST['weight'];
         $sex = $_POST['sex'];
-
+        $petId = 0;
         $query = "SELECT * FROM patient_details WHERE client_id = '$clientId' AND pet_name = '$name'";
         $patient = $con->query($query) or die($con->error);
-        if(!($row = $patient->fetch_assoc())){
+        if($row = $patient->fetch_assoc()){
+            $petId = $row['id'];
+        }else{
             $query = "INSERT INTO patient_details(`client_id`,`pet_name`,`breed`,`species`,`sex`,`created_at`,`updated_at`)VALUES('$clientId','$name','$breed','$species','$sex','$today','$today')";
             $con->query($query) or die($con->error);
+            $query = "SELECT * FROM patient_details WHERE id = LAST_INSERT_ID()";
+            $pet = $con->query($query) or die($con->error);
+            $petRow = $pet->fetch_assoc();
+            $petId = $petRow['id'];
         }
 
-        $query = "INSERT INTO pet_age_and_sexes(`weight`,`age`,`created_at`,`updated_at`,`appointment_id`)VALUES('$weight','$age','$today','$today','$appointmentId')";
+        $query = "INSERT INTO pet_age_and_sexes(`pet_id`,`weight`,`age`,`created_at`,`updated_at`,`appointment_id`)VALUES('$petId','$weight','$age','$today','$today','$appointmentId')";
         $con->query($query) or die($con->error);
         echo 'ok';
     }
