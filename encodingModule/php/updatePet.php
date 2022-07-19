929b7eaa3e12a73a878c8f5c6cfaf61b6f6f5821
pandesalNaSunog <floresjem8@gmail.com>
2022-07-19 15:39:49 +0800

    latest

---
+++ encodingModule/php/updatePet.php
@@ -0,0 +1,22 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        include('connection.php');
+        $con = connect();
+        if(isset($_POST)){
+            $petId = htmlspecialchars($_POST['pet_id']);
+            $name = htmlspecialchars($_POST['name']);
+            $breed = htmlspecialchars($_POST['breed']);
+            $species = htmlspecialchars($_POST['species']);
+            $sex = htmlspecialchars($_POST['sex']);
+
+
+
+            $query = "UPDATE pets SET name = '$name', breed = '$breed', species = '$species', sex = '$sex' WHERE id = '$petId'";
+            $con->query($query) or die($con->error);
+
+            echo 'ok';
+        }
+    }else{
+        echo header('HTTP/1.1 403 Forbidden');
+    }
+?>
