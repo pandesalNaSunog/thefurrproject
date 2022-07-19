f798cca56e83e26eff3ff2147ca76edeb8940f73
pandesalNaSunog <floresjem8@gmail.com>
2022-07-19 19:06:44 +0800

    latest

--- createAdminCreds.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    $con = new mysqli("localhost","u568496919_thefurr","Thefurrpassword11","u568496919_thefurr_db");
-
-    $password = password_hash('password', PASSWORD_DEFAULT);
-    $query = "INSERT INTO users(`name`,`email`,`password`,`user_type`)VALUES('Administrator','admin@gmail.com','$password','admin')";
-    $con->query($query) or die($con->error);
-?>
