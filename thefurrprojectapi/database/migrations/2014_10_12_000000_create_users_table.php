0b19b12decb67a32ed27d32849d75b5d3e559ef8
pandesalNaSunog <floresjem8@gmail.com>
2022-07-20 14:12:58 +0800

    latest

--- thefurrprojectapi/database/migrations/2014_10_12_000000_create_users_table.php
+++ thefurrprojectapi/database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->nullable();
             $table->string('client_code');
-            $table->string('contact_no');
+            $table->string('contact_no')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->string('user_type');
