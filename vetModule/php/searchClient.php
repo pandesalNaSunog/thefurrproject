42f2728de4a007b965507389535f6e62589a094b
pandesalNaSunog <floresjem8@gmail.com>
2022-08-02 15:45:18 +0800

    latest

--- vetModule/php/searchClient.php
+++ vetModule/php/searchClient.php
@@ -6,12 +6,37 @@
         if(isset($_POST) && isset($_SESSION['doctor_id'])){
             $doctor_id = $_SESSION['doctor_id'];
             $keyword = htmlspecialchars($_POST['keyword']);
-            $query = "SELECT * FROM users WHERE user_type = 'client' AND (client_code LIKE '%$keyword%' OR name LIKE '%$keyword%') ORDER BY name ASC";
-            $user = $con->query($query) or die($con->error);
             $users = array();
+            $query = "SELECT * FROM wellness_records WHERE doctor_id = '$doctor_id'";
+            $wellness = $con->query($query) or die($con->error);
+            $petIds = array();
+            $userIds = array();
+            while($wellnessRow = $wellness->fetch_assoc()){
+                $petIds[] = $wellnessRow['pet_id'];
+            }
 
-            while($row = $user->fetch_assoc()){
-                $users[] = $row;
+            foreach($petIds as $petId){
+                $query = "SELECT * FROM pets WHERE id = '$petId'";
+                $pet = $con->query($query) or die($con->error);
+                while($petRow = $pet->fetch_assoc()){
+                    $userIds[] = $petRow['user_id'];
+                }
+            }
+            $currentUserId = 0;
+            foreach($userIds as $userIdItem){
+                if($userIdItem != $currentUserId){
+                    $query = "SELECT * FROM users WHERE id = '$userIdItem' AND user_type ='client' AND name LIKE '%$keyword%' OR client_code LIKE '%$keyword%'";
+                    $userQuery = $con->query($query) or die($con->error);
+                    $userRow = $userQuery->fetch_assoc();
+                    $users[] = array(
+                        'name' => $userRow['name'],
+                        'email' => $userRow['email'],
+                        'client_code' => $userRow['client_code'],
+                        'contact_no' => $userRow['contact_no'],
+                        'id' => $userRow['id'],
+                    );
+                    $currentUserId = $userIdItem;
+                }
             }
 
             echo json_encode($users);
