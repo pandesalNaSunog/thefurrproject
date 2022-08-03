0cb7bb8a8ea3cea931f0cf173c40c96902b9f0ce
pandesalNaSunog <floresjem8@gmail.com>
2022-08-03 13:57:09 +0800

    latest

--- clientModule/php/myPets.php
+++ clientModule/php/myPets.php
@@ -3,6 +3,7 @@
         session_start();
         include('connection.php');
         $con = connect();
+        $today = getCurrentDate();
         if(isset($_SESSION['client_id'])){
             $clientId = $_SESSION['client_id'];
             $query = "SELECT * FROM pets WHERE user_id = '$clientId'";
@@ -10,8 +11,21 @@
             $pets = array();
             while($petRow = $pet->fetch_assoc()){
                 $pets[] = $petRow;
+                $todayObject = date_create($today);
+                $birthdateObject = date_create($petRow['birth_date']);
+
+                $dateDiff = date_diff($todayObject, $birthdateObject);
+
+                $age = $dateDiff->format("%y Year(s) %m Month(s)");
+                $response[] = array(
+                    'name' => $petRow['name'],
+                    'breed' => $petRow['breed'],
+                    'species' => $petRow['species'],
+                    'sex' => $petRow['sex'],
+                    'age' => $age
+                );
             }
-            echo json_encode($pets);
+            echo json_encode($response);
         }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
