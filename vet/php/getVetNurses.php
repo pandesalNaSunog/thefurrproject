44baa09092c46b2a18438d807ee1974c225d949d
pandesalNaSunog <floresjem8@gmail.com>
2022-08-05 12:43:05 +0800

    latest

---
+++ vet/php/getVetNurses.php
@@ -0,0 +1,17 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        include('connection.php');
+        $con = connect();
+        $query = "SELECT * FROM users WHERE user_type = 'vet_nurse'";
+        $user = $con->query($query) or die($con->error);
+
+        $users = array();
+
+        while($row = $user->fetch_assoc()){
+            $users[] = $row;
+        }
+        echo json_encode($users);
+    }else{
+        echo header('HTTP/1.1 403 Forbidden');
+    }
+?>
