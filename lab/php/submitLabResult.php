309d6fb4580208fd7f3679f2a024f73498b54b70
pandesalNaSunog <floresjem8@gmail.com>
2022-08-06 16:48:41 +0800

    latest

--- lab/php/submitLabResult.php
+++ lab/php/submitLabResult.php
@@ -6,7 +6,16 @@
             $result = $_FILES['result']['name'];
             $tmpName = $_FILES['result']['tmp_name'];
             $filExtension = strtolower(pathinfo($result, PATHINFO_EXTENSION));
-            echo $filExtension;
+
+            $allowedExtensions = array("jpg","jpeg","png");
+
+            if(in_array($filExtension, $allowedExtensions)){
+                $filepath = "images/".uniqid().$filExtension;
+                move_uploaded_file($tmpName, $filepath);
+            }else{
+                echo 'invalid file';
+            }
+            
         }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
