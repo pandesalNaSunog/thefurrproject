1bbba79abaa8253142ef70fd6312356d12514575
pandesalNaSunog <floresjem8@gmail.com>
2022-08-06 10:35:47 +0800

    latest

---
+++ thefurrprojectapi/database/migrations/2022_08_06_023221_update_medical.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('medical_records', function(Blueprint $table){
+            $table->string('vet_nurse')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
--- vet/php/addMedicalRecord.php
+++ vet/php/addMedicalRecord.php
@@ -25,8 +25,8 @@
             $service = htmlspecialchars($_POST['service']);
 
 
-            $query = $con->prepare("INSERT INTO medical_records(user_id,doctor_id,pet_id,pet_weight,temp,hr,rr,tests,`procedure`,medication,case_closed,created_at,updated_at)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?)");
-            $query->bind_param("iiissssssssss", $clientId, $doctorId, $petId, $weight, $temp, $hr, $rr, $temp, $procedures,$medication, $caseClosed, $today, $today);
+            $query = $con->prepare("INSERT INTO medical_records(user_id,doctor_id,pet_id,pet_weight,temp,hr,rr,tests,`procedure`,medication,case_closed,created_at,updated_at,vet_nurse)VALUES(?,?,?,?,?,?,?,?,?,?,?,?,?,?)");
+            $query->bind_param("iiisssssssssss", $clientId, $doctorId, $petId, $weight, $temp, $hr, $rr, $temp, $procedures,$medication, $caseClosed, $today, $today,$vetNurse);
             $query->execute();
 
             $newquery = $con->prepare("INSERT INTO wellness_records(pet_id,doctor_id,`service`,remarks,`date`,next_appointment,created_at,updated_at,pet_weight)VALUES(?,?,?,?,?,?,?,?,?)");
