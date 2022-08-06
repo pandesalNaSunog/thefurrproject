b23452a783187c7cf515bfdeaf94bd587a682922
pandesalNaSunog <floresjem8@gmail.com>
2022-08-06 17:46:15 +0800

    latest

--- lab/php/submitLabResult.php
+++ lab/php/submitLabResult.php
@@ -18,7 +18,7 @@
                 move_uploaded_file($tmpName, $filepath);
                 $query = "INSERT INTO lab_results(`lab_request_id`,`result`,`created_at`,`updated_at`)VALUES('$labRequestId','$filepath','$today','$today')";
                 $con->query($query) or die($con->error);
-                $query = "UPDATE lab_requests SET has_result = 'Yes', lab_tech_id = $labTechId WHERE id = '$labRequestId'";
+                $query = "UPDATE lab_requests SET has_result = 'Yes', lab_tech_id = '$labTechId' WHERE id = '$labRequestId'";
                 $con->query($query) or die($con->error);
                 $query = "SELECT * FROM lab_results WHERE id = LAST_INSERT_ID()";
                 $labResultQuery = $con->query($query) or die($con->error);
