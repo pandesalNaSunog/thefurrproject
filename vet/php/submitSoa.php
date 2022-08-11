78266c8132ebd2bd9a94a32c1f1daad82a76a1d1
pandesalNaSunog <floresjem8@gmail.com>
2022-08-11 13:45:03 +0800

    latest

--- vet/php/submitSoa.php
+++ vet/php/submitSoa.php
@@ -45,10 +45,11 @@
 
             foreach($renderedServicesArray as $serviceRendered){
 
-                $thisServiceArray = explode("*", $serviceRendered);
-                $thisService = $thisServiceArray[0];
-                $thisCategory = $thisServiceArray[1];
+                
                 if($serviceRendered != ""){
+                    $thisServiceArray = explode("*", $serviceRendered);
+                    $thisService = $thisServiceArray[0];
+                    $thisCategory = $thisServiceArray[1];
                     $query = "INSERT INTO rendered_services(`category`,`service`,`soa_number`,`lab_tech_id`,`doctor_id`,`created_at`,`updated_at`)VALUES('$thisCategory','$thisService','$soaNumber','0','$doctorId','$today','$today')";
 
                     $con->query($query) or die($con->error);
