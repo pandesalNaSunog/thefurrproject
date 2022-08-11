8323a914cf4fda1ffc29ff745f6415fe94b818a2
pandesalNaSunog <floresjem8@gmail.com>
2022-08-11 17:50:31 +0800

    latest

--- vet/php/getSoa.php
+++ vet/php/getSoa.php
@@ -7,11 +7,33 @@
 
             $query = "SELECT * FROM statement_of_accounts WHERE soa_number = '$soaNumber'";
             $soa = $con->query($query) or die($con->error);
-            $soas = array();
-            while($soaRow = $soa->fetch_assoc()){
-                $soas[] = $soaRow;
+            $response = array();
+            $soaRow = $soa->fetch_assoc();
+
+            $details = $soaRow['details'];
+
+            $detailsArray = explode("**", $details);
+
+            foreach($detailsArray as $details){
+                if($details != ""){
+                    $detailsBreakdown = explode("*", $details);
+
+                    $service = $detailsBreakdown[0];
+                    $basePrice = $detailsBreakdown[2];
+                    $discountedPrice = $detailsBreakdown[3];
+                    $quantity = $detailsBreakdown[4];
+                    $discountRate = $detailsBreakdown[5];
+
+                    $response[] = array(
+                        'service' => $service,
+                        'base_price' => $basePrice,
+                        'discounted_price' => $discountedPrice,
+                        'quantity' => $quantity,
+                        'discount_rate' => $discountRate
+                    );
+                }
             }
-            echo json_encode($soas);
+            echo json_encode($response);
         }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
