082db3d4bd47e478a38b8a4cd62aeb9b49134e2f
pandesalNaSunog <floresjem8@gmail.com>
2022-08-11 13:16:50 +0800

    latest

--- vet/php/getRenderredServices.php
+++ vet/php/getRenderredServices.php
@@ -10,11 +10,36 @@
             $service = $con->query($query) or die($con->error);
             $services = array();
             while($serviceRow = $service->fetch_assoc()){
-                $services[] = $serviceRow;
+
+                $date = date_format(date_create($serviceRow['created_at']), "M d, Y h:i A");
+                $category = $serviceRow['category'];
+                $service = $serviceRow['service'];
+                
+                $labTechId = $serviceRow['lab_tech_id'];
+                if($labTechId == 0){
+                    $labTech = "None";
+                }else{
+                    $query = "SELECT * FROM users WHERE id = '$labTechId'";
+                    $labTechQuery = $con->query($query) or die($con->error);
+                    $labTechRow = $labTechQuery->fetch_assoc();
+
+                    $labTech = $labTechRow['name'];
+                }
+
+                $soaNo = $serviceRow['soa_number'];
+                
+
+                $services[] = array(
+                    'date' => $date,
+                    'category' => $category,
+                    'service' => $service,
+                    'lab_tech' => $labTech,
+                    'soa_no' => $soaNo
+                );
             }
             echo json_encode($services);
         }
-
+        addToServicesTables(index, value.date, value.category, value.service, value.lab_tech, value.soa_no);
     }else{
         echo header('HTTP/1.1 403 Forbidden');
     }
