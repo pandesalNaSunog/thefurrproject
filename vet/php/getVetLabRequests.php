bfda6fb42cdc55450b6c1a33bf63271f0c212152
pandesalNaSunog <floresjem8@gmail.com>
2022-08-12 12:16:10 +0800

    latest

---
+++ vet/php/getVetLabRequests.php
@@ -0,0 +1,85 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        session_start();
+        include('connection.php');
+        $today = getCurrentDate();
+        $con = connect();
+        if(isset($_POST) && isset($_SESSION['doctor_id'])){
+            $doctorId = $_SESSION['doctor_id'];
+            $query = "SELECT * FROM lab_requests WHERE doctor_id = '$doctorId' AND lab_tech_id = '$doctorId'";
+            $request = $con->query($query) or die($con->error);
+            $requests = array();
+
+            while($requestRow = $request->fetch_assoc()){
+                if($requestRow['lab_tech_id'] == 0){
+                    $labTech = 'Pending';
+                }else{
+                    $labTechId = $requestRow['lab_tech_id'];
+                    $query = "SELECT * FROM users WHERE id = '$labTechId'";
+                    $labTechquery = $con->query($query) or die($con->error);
+                    $labTechRow = $labTechquery->fetch_assoc();
+                    $labTech = $labTechRow['name'];
+                }
+
+
+                $petId = $requestRow['pet_id'];
+
+                $query = "SELECT * FROM pets WHERE id = '$petId'";
+                $petQuery = $con->query($query) or die($con->error);
+                $petRow = $petQuery->fetch_assoc();
+                $petName = $petRow['name'];
+
+                $query = "SELECT * FROM users WHERE id = '$doctorId'";
+                $doctorQuery = $con->query($query) or die($con->error);
+                $doctorRow = $doctorQuery->fetch_assoc();
+                $doctor = $doctorRow['name'];
+
+                $labRequestId = $requestRow['id'];
+                $query = "SELECT * FROM lab_results WHERE lab_request_id = '$labRequestId'";
+                $labResultQuery = $con->query($query) or die($con->error);
+                $labResults = array();
+                while($labResultRow = $labResultQuery->fetch_assoc()){
+                    $labResults[] = $labResultRow;
+                }
+
+                if(count($labResults) == 0){
+                    $result = "Pending";
+                }else{
+                    $result = $labResults;
+                }
+
+                $timeLeft = calculateTimeLeft($today, $requestRow['created_at'], $requestRow['time_limit'], $con, $labRequestId);
+
+                $requests[] = array(
+                    'id' => $requestRow['id'],
+                    'time_limit' => $timeLeft."min",
+                    'patient_name' => $petName,
+                    'request' => $requestRow['request'],
+                    'lab_tech' => $labTech,
+                    'result' => $result,
+                    'date' => date_format(date_create($requestRow['created_at']),"M d, Y h:i A"),
+                );
+            }
+            echo json_encode($requests);
+        }else{
+            echo 0;
+        }
+    }else{
+        echo header('HTTP/1.1 403 Forbidden');
+    }
+
+
+
+    function calculateTimeLeft($today, $timeRequested, $timeLimit, $con ,$labRequestId){
+        $expectedTime = strtotime($timeRequested . " + " . $timeLimit . " minute");
+        $dateDiff = date_diff(date_create($today), date_create(date('Y-m-d H:i:s', $expectedTime)));
+        $timeLeft = $dateDiff->format("%i");
+
+        $percentR = $dateDiff->format("%R");
+
+        if($percentR == "-"){
+            $timeLeft = 0;
+        }
+        return $timeLeft;
+    }
+?>
---
+++ vet/php/submitLabResult.php
@@ -0,0 +1,36 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        session_start();
+        include('connection.php');
+        $con = connect();
+        $today = getCurrentDate();
+        if(isset($_FILES) && isset($_SESSION['lab_tech_id'])){
+            $labRequestId = $_POST['lab_request_id'];
+            $labTechId = $_SESSION['lab_tech_id'];
+            $result = $_FILES['result']['name'];
+            $tmpName = $_FILES['result']['tmp_name'];
+            $filExtension = strtolower(pathinfo($result, PATHINFO_EXTENSION));
+
+            $allowedExtensions = array("jpg","jpeg","png","pdf");
+
+            if(in_array($filExtension, $allowedExtensions)){
+                $filepath = "../../lab/php/images/".uniqid().".".$filExtension;
+                move_uploaded_file($tmpName, $filepath);
+                $filepath = str_replace("../../lab/php/","",$filepath);
+                $query = "INSERT INTO lab_results(`lab_request_id`,`result`,`created_at`,`updated_at`)VALUES('$labRequestId','$filepath','$today','$today')";
+                $con->query($query) or die($con->error);
+                $query = "UPDATE lab_requests SET has_result = 'Yes', time_limit = 0 WHERE id = '$labRequestId'";
+                $con->query($query) or die($con->error);
+                $query = "SELECT * FROM lab_results WHERE id = LAST_INSERT_ID()";
+                $labResultQuery = $con->query($query) or die($con->error);
+                $labResultRow = $labResultQuery->fetch_assoc();
+                echo json_encode($labResultRow);
+            }else{
+                echo 'invalid file';
+            }
+            
+        }
+    }else{
+        echo header('HTTP/1.1 403 Forbidden');
+    }
+?>
