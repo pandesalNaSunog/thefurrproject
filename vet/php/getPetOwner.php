3ec067727220ffb539334465dfe5252ae8dbb4f6
pandesalNaSunog <floresjem8@gmail.com>
2022-08-12 17:31:37 +0800

    latest

--- vet/php/getPetOwner.php
+++ vet/php/getPetOwner.php
@@ -5,38 +5,37 @@
 
         if(isset($_POST)){
             $petIdArray = explode("*",$_POST['pet_id']);
-            // $petId = $petIdArray[0];
-            // $appointmentId = $petIdArray[1];
+            $petId = $petIdArray[0];
+            $appointmentId = $petIdArray[1];
 
-            // $hasMedicalRecord = false;
-            // $query = "SELECT * FROM medical_records WHERE pet_id = '$petId' AND appointment_id = '$appointmentId'";
-            // $medicalRecord = $con->query($query) or die($con->error);
-            // if($medicalRecordRow = $medicalRecord->fetch_asso()){
-            //     $hasMedicalRecord = true;
-            // }else{
-            //     $hasMedicalRecord = false;
-            // }
-            // $query = "SELECT * FROM pets WHERE id = '$petId'";
-            // $pet = $con->query($query) or die($con->error);
-            // $petRow = $pet->fetch_assoc();
-            // $petName = $petRow['name'];
-            // $clientId = $petRow['user_id'];
-            // $query = "SELECT * FROM users WHERE id = '$clientId'";
-            // $user = $con->query($query) or die($con->error);
+            $hasMedicalRecord = false;
+            $query = "SELECT * FROM medical_records WHERE pet_id = '$petId' AND appointment_id = '$appointmentId'";
+            $medicalRecord = $con->query($query) or die($con->error);
+            if($medicalRecordRow = $medicalRecord->fetch_asso()){
+                $hasMedicalRecord = true;
+            }else{
+                $hasMedicalRecord = false;
+            }
+            $query = "SELECT * FROM pets WHERE id = '$petId'";
+            $pet = $con->query($query) or die($con->error);
+            $petRow = $pet->fetch_assoc();
+            $petName = $petRow['name'];
+            $clientId = $petRow['user_id'];
+            $query = "SELECT * FROM users WHERE id = '$clientId'";
+            $user = $con->query($query) or die($con->error);
 
-            // $userRow = $user->fetch_assoc();
-            // $name = $userRow['name'];
-            // $code = $userRow['client_code'];
-            // $response = array(
-            //     'client_id' => $clientId,
-            //     'name' => $name,
-            //     'pet_name' => $petName,
-            //     'client_code' => $code,
-            //     'has_medical_record' => $hasMedicalRecord
-            // );
+            $userRow = $user->fetch_assoc();
+            $name = $userRow['name'];
+            $code = $userRow['client_code'];
+            $response = array(
+                'client_id' => $clientId,
+                'name' => $name,
+                'pet_name' => $petName,
+                'client_code' => $code,
+                'has_medical_record' => $hasMedicalRecord
+            );
 
-            // echo json_encode($response);
-            echo json_encode($petIdArray);
+            echo json_encode($response);
         }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
