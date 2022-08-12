ab01299e8ca25229290327c7fec5b2dd5da783fd
pandesalNaSunog <floresjem8@gmail.com>
2022-08-12 15:38:55 +0800

    latest

--- cashier/php/getSoaDetails.php
+++ cashier/php/getSoaDetails.php
@@ -9,8 +9,35 @@
             $query = "SELECT * FROM statement_of_accounts WHERE id = '$soaId'";
             $soa = $con->query($query) or die($con->error);
             $soaRow = $soa->fetch_assoc();
+            $response = array();
+            $soaDetails = $soaRow['details'];
 
-            echo json_encode($soaRow);
+            $soaDetailsArray = explode("**", $soaDetails);
+
+            foreach($soaDetailsArray as $soaDetailsItem){
+                if($soaDetailsItem != ""){
+                    $soaDetailsBreakdown = explode("*", $soaDetailsItem);
+
+                    $service = $soaDetailsBreakdown[0];
+                    $basePrice = $soaDetailsBreakdown[2];
+                    $amount = $soaDetailsBreakdown[2] * $soaDetailsBreakdown[4];
+                    $quantity = $soaDetailsBreakdown[4];
+                    $discounted = $soaDetailsBreakdown[3];
+                    $discount = $soaDetailsBreakdown[6];
+
+
+                    $response[] = array(
+                        'service' => $service,
+                        'base_price' => $basePrice,
+                        'amount' => $amount,
+                        'quantity' => $quantity,
+                        'discounted' => $discounted,
+                        'discount' => $discount
+                    );
+                }
+            }
+
+            echo json_encode($response);
         }
     }else{
         echo header('HTTP/1.1 403 Forbidden');
