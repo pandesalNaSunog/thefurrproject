050a552c36b5ae4487756cd0f8c05e794e1f2254
pandesalNaSunog <floresjem8@gmail.com>
2022-08-13 13:45:32 +0800

    latest

---
+++ cashier/php/getTodaysAppointments.php
@@ -0,0 +1,22 @@
+<?php
+    if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest'){
+        date_default_timezone_set('Asia/Manila');
+        $today('Y-m-d');
+
+        include('connection.php');
+        $con = connect();
+
+        $query = "SELECT * FROM appointments WHERE created_at LIKE '%$today%'";
+
+        $appointment = $con->query($query) or die($con->error);
+        $appointments = array();
+
+        while($appointmentRow = $appointment->fetch_assoc()){
+            $appointments[] = $appointmentRow;
+        }
+
+        echo json_encode($appointments);
+    }else{
+        echo header('HTTP/1.1 403 Forbidden');
+    }
+?>
