056ad6e4b72632d86d556062382347361adfcc20
pandesalNaSunog <floresjem8@gmail.com>
2022-08-13 14:49:10 +0800

    latest

--- cashier/php/getTodaysAppointments.php
+++ cashier/php/getTodaysAppointments.php
@@ -11,36 +11,41 @@
         $appointment = $con->query($query) or die($con->error);
         $appointments = array();
 
-        while($appointmentRow = $appointment->fetch_assoc()){
-            $date = date_format(date_create($appointmentRow['date']), "M d, Y");
-            $doctorIds = $appointmentRow['doctor_id'];
-            $petIds = $appointmentRow['pet_ids'];
+        $appointmentRow = $appointment->fetch_assoc();
 
-            $doctorIdArray = explode("*", $doctorIds);
-            $petIdArray = explode("*", $petIds);
+        $doctorIdArray = explode("*", $appointmentRow['doctor_id']);
+        echo json_encode($doctorIdArray);
 
-            foreach($doctorIdArray as $key => $doctorId){
-                $query = "SELECT * FROM users WHERE id = '$doctorId'";
-                $doctor = $con->query($query) or die($con->error);
-                $doctorRow = $doctor->fetch_assoc();
-                $doctorName = $doctorName['name'];
+        // while($appointmentRow = $appointment->fetch_assoc()){
+        //     $date = date_format(date_create($appointmentRow['date']), "M d, Y");
+        //     $doctorIds = $appointmentRow['doctor_id'];
+        //     $petIds = $appointmentRow['pet_ids'];
 
-                $petId = $petIdArray[$key];
-                $query = "SELECT * FROM pets WHERE id = '$petId'";
-                $pet = $con->query($query) or die($con->error);
-                $petRow = $pet->fetch_assoc();
-                $petName = $petRow['name'];
+        //     $doctorIdArray = explode("*", $doctorIds);
+        //     $petIdArray = explode("*", $petIds);
 
-                $appointments[] = array(
-                    'date' => $date,
-                    'doctor' => $doctorName,
-                    'pet' => $petName
-                );
-            }
-            echo json_encode($appointments);
-        }
+        //     foreach($doctorIdArray as $key => $doctorId){
+        //         $query = "SELECT * FROM users WHERE id = '$doctorId'";
+        //         $doctor = $con->query($query) or die($con->error);
+        //         $doctorRow = $doctor->fetch_assoc();
+        //         $doctorName = $doctorName['name'];
 
-        echo json_encode($appointments);
+        //         $petId = $petIdArray[$key];
+        //         $query = "SELECT * FROM pets WHERE id = '$petId'";
+        //         $pet = $con->query($query) or die($con->error);
+        //         $petRow = $pet->fetch_assoc();
+        //         $petName = $petRow['name'];
+
+        //         $appointments[] = array(
+        //             'date' => $date,
+        //             'doctor' => $doctorName,
+        //             'pet' => $petName
+        //         );
+        //     }
+        //     echo json_encode($appointments);
+        // }
+
+        // echo json_encode($appointments);
     }else{
         echo header('HTTP/1.1 403 Forbidden');
     }
