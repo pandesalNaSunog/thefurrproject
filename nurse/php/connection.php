00c6d94f4c2fd18eb97b7259f433e202fefa644b
pandesalNaSunog <floresjem8@gmail.com>
2022-11-04 11:59:20 +0800

    latest

--- nurse/php/connection.php
+++ nurse/php/connection.php
@@ -3,7 +3,7 @@
     if(isSecure()){
         
         function connect(){
-            //return new mysqli("localhost","u568496919_thefurr","Thefurrpassword11","u568496919_thefurr_db");
+            return new mysqli("localhost","u568496919_thefurr","Thefurrpassword11","u568496919_thefurr_db");
             return new mysqli("localhost","root","","thrfurr_db");
         }
 
